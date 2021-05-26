a140b727658d5b770075baddd10061a7d0c84c55
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-26 16:43:41 +0900

    updates

--- src/components/project/RadGrad.jsx
+++ src/components/project/RadGrad.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header, Container, Grid } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 function RadGrad2() {
   return (
@@ -7,9 +8,10 @@ function RadGrad2() {
         <br/>
         <Header as ="h2" textAlign="center">RadGrad 2.0</Header>
         <Grid textAlign = "center">
-          <p textAlign="center"> <b> Software Engineering, Meteor, React </b> <br/>
-            <span className="bold-list"> On Going</span> </p>
-          <br/>
+          <p textAlign="center"> <b> Software Engineering, Meteor, React </b> <br/></p>
+        </Grid>
+        <Grid textAlign="center">
+          <p><span className="bold-list"> On Going</span> </p>
         </Grid>
         <Container text>
           <hr/>
@@ -22,6 +24,16 @@ function RadGrad2() {
           <ul>
             <li> For the repository <a href="https://github.com/radgrad/radgrad2">page</a></li>
             <li> For the organization <a href={"https://www.radgrad.org/"}>page</a></li>
+          </ul>
+          <b><u>Week 1 Tasks</u></b><br/>
+          For this project, we used the GitHub Project <i>Kanban</i> Board to list the different issues that need to be worked on.
+          In the first week, the intern group concentrated on the welcoming task which centered around on reviewing the rules and conducts,
+          the research progress, and some review or learning Javascript and TypeScript.<br/>
+          Additionally, for the first week issues, we were asked to write essays on the different topics. Below here are the link to the essays.
+          <ul>
+            <li><NavLink exact to = "/radgradreview">RadGrad Reivew </NavLink></li>
+            <li><NavLink exact to = "/radgradfeedback">Comments on the Current RadGrad</NavLink></li>
+            <li><NavLink exact to = "/radgradstart">Review and Writing Code</NavLink></li>
           </ul>
 
 
