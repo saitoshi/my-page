79cd5798022ad9bd9d2141124a33ceb4b16c6eb5
Shun <47809066+saitoshi@users.noreply.github.com>
2021-10-28 13:21:05 +0900

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { Header, Container, Card, Image, Button, Grid } from 'semantic-ui-react';
 import { NavLink } from "react-router-dom";
-const shockingLogo = require('./images/shocking.png');
 
 function Project() {
   return (
       <Container>
+        <br/>
         <Header as ="h2" textAlign="center">PROJECT</Header>
+        <div className='comment'>Click on the cards if you would like to read more on the project.</div>
         <Container text>
         <hr/>
-<<<<<<< Updated upstream
-        <Card.Group itemsPerRow={3}>
-          <Card>
-            <Image src="https://saitoshi.github.io/images/RadGrad.png"/>
-=======
         <Card.Group itemsPerRow={3} centered>
+          <Card as = {NavLink} exact to = "/crosscountry">
+            <Image src="https://github.com/saitoshi/images/blob/main/algotrix.png?raw=true"/>
+            <Card.Content>
+              <Card.Header textAlign='center'>Algo-Trix</Card.Header>
+              <Card.Meta textAlign='center'>
+                <span className='date'>ON GOING</span>
+              </Card.Meta>
+              <Card.Description>
+                A Meteor and React based application created for ICS 427 Summer 2021 that provides college students additional practice for Algorithm outside of a textbook.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              Software Engineering, React, Software Quality Assurance <br/>
+            </Card.Content>
+          </Card>
           <Card as = {NavLink} exact to = "/radgrad">
             <Image src="https://saitoshi.github.io/images/RadGrad.png"/>
             <Card.Content>
               <Card.Header textAlign='center'>RadGrad 2.0</Card.Header>
               <Card.Meta textAlign='center'>
-                <span className='date'>2021-07-30</span>
+                <span className='date'>ON GOING</span>
               </Card.Meta>
               <Card.Description>
                 A Meteor and React based planner targeted for Computer Science and Electrical Engineering students that students can do based on their Career Goals.
@@ -30,38 +41,7 @@ function Project() {
               Software Engineering, Meteor, React
             </Card.Content>
           </Card>
-          <Card as = {NavLink} exact to = "/crosscountry">
-            <Image src="https://github.com/saitoshi/images/blob/main/algotrix.png?raw=true"/>
->>>>>>> Stashed changes
-            <Card.Content>
-              <Card.Header textAlign='center'>Algo-Trix</Card.Header>
-              <Card.Meta textAlign='center'>
-                <span className='date'>2020-07-02</span>
-              </Card.Meta>
-              <Card.Description>
-<<<<<<< Updated upstream
-                A project created during my Summer 2021 internship program.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              Software Engineering, Meteor, React <br/>
-              <br/>
-              <Grid>
-                <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/radgrad">MORE DETAILS</Button>
-                </Grid.Column>
-              </Grid>
-=======
-                A Meteor and React based application created for ICS 427 Summer 2021 that provides college students additional practice for Algorithm outside of a textbook.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              Software Engineering, React, Software Quality Assurance <br/>
->>>>>>> Stashed changes
-            </Card.Content>
-          </Card>
-
-          <Card>
+          <Card as = {NavLink} exact to = "/shocking">
             <Image src="https://saitoshi.github.io/images/shocking.png" />
             <Card.Content>
               <Card.Header textAlign='center'>Shocking Emissions</Card.Header>
@@ -73,17 +53,11 @@ function Project() {
               </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              Software Engineering, Meteor, React <br/>
-              <br/>
-              <Grid>
-                <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/shocking">MORE DETAILS</Button>
-                </Grid.Column>
-              </Grid>
+              Software Engineering, Meteor, React
             </Card.Content>
           </Card>
 
-          <Card>
+          <Card as = {NavLink} exact to = "/tregear">
             <Image src="https://saitoshi.github.io/images/tregear.png" />
             <Card.Content>
               <Card.Header textAlign='center'>Tregear Story Translation</Card.Header>
@@ -94,17 +68,11 @@ function Project() {
                 A Japanese to English translation for the short story called "Tregear Story" written by NAKANO Takao.              </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              Japanese to English Translation<br/>
-              <br/>
-              <Grid>
-                <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/tregear">MORE DETAILS</Button>
-                </Grid.Column>
-              </Grid>
+              Japanese to English Translation
             </Card.Content>
           </Card>
-          <Card>
-            <Image src="https://saitoshi.github.io/images/textbookhunt.png" />
+          <Card as = {NavLink} exact to = "/uhmath">
+            <Image src="https://saitoshi.github.io/images/math.jpeg" />
             <Card.Content>
               <Card.Header textAlign='center'>UH Math Review</Card.Header>
               <Card.Meta textAlign='center'>
@@ -115,17 +83,11 @@ function Project() {
               </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              Mathematics, HTML, LaTex <br/>
-              <br/>
-              <Grid>
-                <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/textbookhunt">MORE DETAILS</Button>
-                </Grid.Column>
-              </Grid>
+              Mathematics, HTML, LaTex
             </Card.Content>
           </Card>
 
-          <Card>
+          <Card as = {NavLink} exact to = "/textbookhunt">
             <Image src="https://saitoshi.github.io/images/textbookhunt.png" />
             <Card.Content>
               <Card.Header textAlign='center'>Textbook Hunt</Card.Header>
@@ -137,31 +99,45 @@ function Project() {
             </Card.Content>
             <Card.Content extra>
               Software Engineering, Meteor, React<br/>
-              <br/>
+            </Card.Content>
+          </Card>
+
+          <Card>
+            <Image src="https://saitoshi.github.io/images/mathchallenge.jpg" />
+            <Card.Content>
+              <Card.Header textAlign='center'>Hawaii Math State Challenge</Card.Header>
+              <Card.Meta textAlign='center'>
+                <span className='date'>2020-02-15</span>
+              </Card.Meta>
+              <Card.Description>
+                A project event where we did a presentation on the different possible ways of fixing the island of Kahoolawe.             </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              Mathematics, Education, Academic Event
               <Grid>
                 <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/textbookhunt">MORE DETAILS</Button>
+                  <Button as = {NavLink} exact to = "/math">MORE DETAILS</Button>
                 </Grid.Column>
               </Grid>
             </Card.Content>
           </Card>
 
           <Card>
-            <Image src="https://saitoshi.github.io/images/textbookhunt.png" />
+            <Image src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/03/Toei-Tokusatsu-header.png" />
             <Card.Content>
-              <Card.Header textAlign='center'>Textbook Hunt</Card.Header>
+              <Card.Header textAlign='center'>Japanese Tokusatsu Paper</Card.Header>
               <Card.Meta textAlign='center'>
-                <span className='date'>2020-05-15</span>
+                <span className='date'>2019-05-15</span>
               </Card.Meta>
               <Card.Description>
-                A meteor and react based application where students can sell used textbooks to others               </Card.Description>
+                A final research paper written centering around the history of Japanese Special Effect Shows <i>Tokusatsu</i> for JPN 421: Japanese Composition.
+              </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              Software Engineering, Meteor, React<br/>
-              <br/>
+              Japanese, Research Paper
               <Grid>
                 <Grid.Column textAlign="center">
-                  <Button as = {NavLink} exact to = "/textbookhunt">MORE DETAILS</Button>
+                  <Button as = {NavLink} exact to = "/tokusatsu">MORE DETAILS</Button>
                 </Grid.Column>
               </Grid>
             </Card.Content>
@@ -170,6 +146,7 @@ function Project() {
         </Card.Group>
 
         </Container>
+        <div className="push"/>
       </Container>
 
 
